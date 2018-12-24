dd045eb210ac842fd0690992787d24d3356d2ccc
d <45852339+dangty253@users.noreply.github.com>
2018-12-24 12:10:20 +0700

    add v1

---
+++ admins/xulymaybay.php
@@ -0,0 +1,26 @@
+<?php
+include "../config/config.php";
+include ROOT."/include/function.php";
+if (!isset($_SESSION)) session_start();
+spl_autoload_register("loadClass");
+
+$c = getIndex("c","");
+$MaMayBay=postIndex("MaMayBay","");
+$MaHangMayBay=postIndex("MaHangMayBay","");
+if($c=='add')
+{
+	$Sb=new Maybay();
+	$Sb->add($MaMayBay,$MaHangMayBay);
+}
+else if($c=='update')
+{
+	$Sb=new Maybay();
+	$Sb->update($MaMayBay,$MaHangMayBay);
+}
+else if($c=='delete')
+{
+	$MaMayBay = getIndex("id","");
+	$Sb=new Maybay();
+	$Sb->delete($MaMayBay);
+}
+echo "<script type='text/javascript'>window.location('index.php?mod=datvemb&group=maybay&c=$c');</script>";
---
+++ admins/xulysanbay.php
@@ -0,0 +1,27 @@
+<?php
+include "../config/config.php";
+include ROOT."/include/function.php";
+if (!isset($_SESSION)) session_start();
+spl_autoload_register("loadClass");
+
+$c = getIndex("c","");
+$MaSanBay = postIndex("MaSanBay","");
+$TenSanBay = postIndex("TenSanBay","");
+$TenTinhThanh = postIndex("TenTinhThanh","");
+if($c=='add')
+{
+	$Sb=new Sanbay();
+	$Sb->add($MaSanBay,$TenTinhThanh,$TenSanBay);
+}
+else if($c=='update')
+{
+	$Sb=new Sanbay();
+	$Sb->update($MaSanBay,$TenTinhThanh,$TenSanBay);
+}
+else if($c=='delete')
+{
+	$MaSanBay = getIndex("id","");
+	$Sb=new Sanbay();
+	$Sb->delete($MaSanBay);
+}
+echo "<script type='text/javascript'>window.location('index.php?mod=datvemb&group=sanbay&c=$c');</script>";
